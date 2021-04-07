55e7b4973852269d721311af1ba8753f89a3532d
Tanmay <tanmay@Tanmays-MacBook-Air.local>
2021-04-08 00:22:45 +0530

    able to get dom, need to send it to api

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,38 +1,12 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-// //example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//   console.log(`request, sender, sendResponse`);
-//   chrome.pageAction.show(sender.tab.id);
-//   sendResponse();
-// });
-
-// https://developer.chrome.com/extensions/getstarted
 chrome.runtime.onInstalled.addListener(function() {
-  // chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
-  //   var url = tabs[0].url;
-  //   console.log(`tabs`, tabs);
-  // });
-  // chrome.tabs.query(
-  //   { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-  //   function(tabs) {
-  //     console.log(`tabs`, tabs);
-  //   }
-  // );
-  // chrome.storage.sync.set({ color: "#3aa757" }, function() {
-  //   console.log("The color is green.");
-  // });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "github.com" }
+            pageUrl: { hostEquals: "resdex.naukri.com" }
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -51,25 +25,18 @@ chrome.runtime.onInstalled.addListener(function() {
       });
     });
 
-    // chrome.tabs.query(
-    //   { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-    //   function(tabs) {
-    //     // const url = document
-    //     //   .querySelector('meta[name="go-import"]')
-    //     //   .content.split(" ")[0];
-    //     // console.log(`go-import`, url);
-    //     // // chrome.storage.sync.set({ url: tabs[0].url }, function() {});
-
-    //     chrome.tabs.executeScript(
-    //       tabs[0].id,
-    //       {
-    //         code: `document.querySelector('meta[name="go-import"]').content.split(' ')[0]`
-    //       },
-    //       function(url) {
-    //         chrome.storage.sync.set({ url }, function() {});
-    //       }
-    //     );
-    //   }
-    // );
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  (request, sender, senderResponse) => {
+    switch (request.message) {
+      case 'save_text': {
+        console.log('testing',request.data)
+        //saveExtractedText(request.data);
+        break;
+      }
+      default:
+    }
+  }
+);
--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -53,66 +53,45 @@ chrome.tabs.query(
 function setupForkSync(tabId) {
   // @TODO: use "octolytics-dimension-repository_is_fork" to check if repo is a fork later
   const code = `(function getUrls(){
-      const forkUrl = document.querySelector('meta[name="octolytics-dimension-repository_parent_nwo"]') 
-        ? document.querySelector('meta[name="octolytics-dimension-repository_parent_nwo"]').content
+      const forkUrl = document.querySelector('.exp') 
+        ? document.querySelector('.exp').textContent
         : undefined;
 
-      const url = document.querySelector('meta[name="go-import"]') 
-        ? document.querySelector('meta[name="go-import"]').content.split(' ')[0]
-        : undefined;
 
-      return { forkUrl, url };
+      return { forkUrl};
     })()`;
 
   // http://infoheap.com/chrome-extension-tutorial-access-dom/
   chrome.tabs.executeScript(tabId, { code }, function(result) {
-    const { forkUrl, url } = result[0];
+    const { forkUrl} = result[0];
 
-    if (forkUrl) {
-      forkSync.innerText = `git remote add upstream https://github.com/${forkUrl}
-git fetch upstream
-git branch --set-upstream-to=upstream/master master`;
-    } else if (url) {
-      forkSync.innerText = `git remote add upstream https://${url}
-git fetch upstream
-git branch --set-upstream-to=upstream/master master`;
-    } else {
-      forkSync.innerText = `Nothing to do here - Not a Forked Repo`;
-      copyForkSyncButton.style.visibility = "hidden";
-    }
-
+      forkSync.innerText = `Test2 : ${forkUrl}`;
+      chrome.runtime.sendMessage({ message: 'save_text',forkUrl });
     // forkSync.innerText = JSON.stringify(result);
   });
 }
 
+
+
 function setupLocalPR(tabId) {
   // <meta property="og:url" content="https://github.com/dance2die/calendar-dates/pull/62">
   const code = `(function getPRId() {
-    const url = document.querySelector('meta[property="og:url"]')
-      ? document.querySelector('meta[property="og:url"]').content
+    const url = document.querySelector('a[class="userName name"]')
+      ? document.querySelector('a[class="userName name"]').textContent
       : undefined;
-    const tokens = url.split("/");
-    const prId = tokens[tokens.length - 1];
-
-    return url ? prId : undefined;
+    return {url}; 
   })()`;
 
   chrome.tabs.executeScript(tabId, { code }, function(result) {
-    const prId = result[0];
+    const {prId} = result[0];
 
-    const isPR = !isNaN(parseInt(prId));
+    //const isPR = !isNaN(parseInt(prId));
 
     chrome.storage.sync.get("branchName", function({ branchName }) {
       // // @ToDo: Get this from user's preference
-      // const branchName = "BRANCHNAME";
 
-      if (isPR) {
-        localPR.innerText = `git fetch origin pull/${prId}/head:${branchName}
-git checkout ${branchName}`;
-      } else {
-        localPR.innerText = `Nothing to do here - Not a Pull Request page`;
-        copyLocalPRButton.style.visibility = "hidden";
-      }
+
+        localPR.innerText = `Test${prId}`;
     });
   });
 }
